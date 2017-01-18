e3d4316a4e0a3e1c3896ef7aa0de9c901b09a75e
Nick Lalic <nick.lalic@gmail.com>
2017-01-18 00:18:33 -0800

    WIP: Provisioning

--- provision/linux.sh
+++ provision/linux.sh
@@ -11,6 +11,9 @@ sudo apt-get update
 # Install pyenv
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+export PATH="~/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 echo "export PATH=\"~/.pyenv/bin:\$PATH\"" >> ~/.bashrc
 echo "eval \"\$(pyenv init -)\"" >> ~/.bashrc
 echo "eval \"\$(pyenv virtualenv-init -)\"" >> ~/.bashrc

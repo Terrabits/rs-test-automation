1187576469f8ea26ae67216529617f96508a23f8
Nicholas Lalic <nicholaslalic@bwipasminis-Mac-mini.local>
2017-02-05 15:43:35 -0800

    Improved provision scripts, changed project name for SNP ONLY

--- provision/linux.sh
+++ provision/linux.sh
@@ -69,7 +69,7 @@ bundle install
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 # Try to load nvm into bash now:
 # (not sure if this is working...)
-export NVM_DIR="~/.nvm"
+export NVM_DIR=~/.nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 # Install Node
